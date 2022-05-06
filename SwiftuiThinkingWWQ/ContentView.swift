a0a1436bc2ce4d32fa5c2f8180a848be9e58eb68
wwq345 <wwq.bst@sap.com>
2022-05-06 15:08:14 +0800

    Initial Commit

---
+++ SwiftuiThinkingWWQ/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftuiThinkingWWQ
+//
+//  Created by i564206 on 2022/5/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftuiThinkingWWQ/SwiftuiThinkingWWQApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiThinkingWWQApp.swift
+//  SwiftuiThinkingWWQ
+//
+//  Created by i564206 on 2022/5/6.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiThinkingWWQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
